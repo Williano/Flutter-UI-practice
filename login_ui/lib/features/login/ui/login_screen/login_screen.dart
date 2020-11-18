e32b2a7a42a47f804aac47175cce968ccaf9a057
Williano <paawilly17@gmail.com>
2020-11-17 22:12:19 -0600

    Add UI structure

--- login_ui/lib/features/login/ui/login_screen/login_screen.dart
+++ login_ui/lib/features/login/ui/login_screen/login_screen.dart
@@ -10,6 +10,29 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back_ios),
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: Container(
+          height: MediaQuery.of(context).size.height / 3,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.blue,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(Icons.ac_unit),
+              Container(
+                width: MediaQuery.of(context).size.width / 1.2,
+                height: MediaQuery.of(context).size.height / 4,
+                color: Colors.white,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
