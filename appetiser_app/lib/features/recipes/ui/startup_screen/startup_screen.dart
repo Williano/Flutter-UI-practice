5e0dbda70faf8dd2befc6b01a212b54331d9d8dd
Williano <paawilly17@gmail.com>
2020-12-01 17:20:48 -0600

    Add background image

--- appetiser_app/lib/features/recipes/ui/startup_screen/startup_screen.dart
+++ appetiser_app/lib/features/recipes/ui/startup_screen/startup_screen.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 class StartupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: [
+            Image.asset(
+              "assets/images/food.jpg",
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              fit: BoxFit.cover,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
