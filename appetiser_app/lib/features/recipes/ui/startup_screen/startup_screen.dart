ca21ebbf215dffad3fcc49d78c09826a86bad74a
Williano <paawilly17@gmail.com>
2020-12-01 18:52:14 -0600

    Finish startup screen

--- appetiser_app/lib/features/recipes/ui/startup_screen/startup_screen.dart
+++ appetiser_app/lib/features/recipes/ui/startup_screen/startup_screen.dart
@@ -12,7 +12,87 @@ class StartupScreen extends StatelessWidget {
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height,
               fit: BoxFit.cover,
-            )
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              color: Colors.grey[900].withOpacity(0.6),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height / 1.6,
+              right: MediaQuery.of(context).size.width / 20,
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Happy Food",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Cooking",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.w300),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Make the most delicious meals with the",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          letterSpacing: 0.5),
+                    ),
+                    Text(
+                      "ingredients you have in your fridge!",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          letterSpacing: 0.5),
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 1.1,
+                      height: 45,
+                      child: FlatButton(
+                        onPressed: () {},
+                        color: Colors.amber[800],
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Find your perfect recipes",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.white,
+                              size: 15,
+                            ),
+                          ],
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0)),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
