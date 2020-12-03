ad186333170b4e176244cbad51c949405b42d886
Williano <paawilly17@gmail.com>
2020-12-02 20:32:39 -0600

    Add recipe list screen

---
+++ appetiser_app/lib/features/recipes/ui/recipe_list_screen/recipe_list_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RecipeListScreen extends StatefulWidget {
+  @override
+  _RecipeListScreenState createState() => _RecipeListScreenState();
+}
+
+class _RecipeListScreenState extends State<RecipeListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
