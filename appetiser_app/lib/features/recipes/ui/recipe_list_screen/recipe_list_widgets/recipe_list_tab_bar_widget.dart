7afa87df078a7b9cd0c540725dd54c9ce121fcde
Williano <paawilly17@gmail.com>
2020-12-03 21:32:10 -0600

    Add tabbar

---
+++ appetiser_app/lib/features/recipes/ui/recipe_list_screen/recipe_list_widgets/recipe_list_tab_bar_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class RecipeListTabBarWidget extends StatelessWidget {
+  const RecipeListTabBarWidget({
+    Key key,
+    @required TabController tabcontroller,
+  })  : _tabcontroller = tabcontroller,
+        super(key: key);
+
+  final TabController _tabcontroller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      labelColor: Colors.black,
+      unselectedLabelColor: Colors.grey[600],
+      indicator: UnderlineTabIndicator(
+          borderSide: BorderSide(
+            width: 3,
+            color: Colors.yellow[800],
+          ),
+          insets: EdgeInsets.symmetric(horizontal: 35.0)),
+      indicatorWeight: 3,
+      controller: _tabcontroller,
+      labelStyle: TextStyle(color: Colors.black),
+      tabs: [
+        Tab(
+          text: "All recipes",
+        ),
+        Tab(
+          text: "Brekkie",
+        ),
+        Tab(
+          text: "Lunch",
+        ),
+        Tab(
+          text: "Dinner",
+        )
+      ],
+    );
+  }
+}
