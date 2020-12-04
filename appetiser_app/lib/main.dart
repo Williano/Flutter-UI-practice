eb16e8ec9847f35234f464c2a819d9a0bc9ac66b
Williano <paawilly17@gmail.com>
2020-12-04 00:38:38 -0600

    Update

--- appetiser_app/lib/main.dart
+++ appetiser_app/lib/main.dart
@@ -3,10 +3,13 @@ import 'package:appetiser_app/features/recipes/ui/home_screen/home_screen.dart';
 import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarIconBrightness: Brightness.dark, statusBarColor: Colors.white));
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   runApp(AppetiserApp());
 }
 
