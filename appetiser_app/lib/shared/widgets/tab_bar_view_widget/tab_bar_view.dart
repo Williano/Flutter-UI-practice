bf92c7ec3f3e89b1ebc05d896326e80c181c064d
Williano <paawilly17@gmail.com>
2020-12-05 00:25:14 -0600

    Refactor

--- appetiser_app/lib/shared/widgets/tab_bar_view_widget/tab_bar_view.dart
+++ appetiser_app/lib/shared/widgets/tab_bar_view_widget/tab_bar_view.dart
@@ -3,20 +3,23 @@ import 'package:flutter/material.dart';
 class TabBarViewWidget extends StatelessWidget {
   final TabController _tabcontroller;
   final List<Widget> _screens;
+  final String _pageStorageKey;
 
-  const TabBarViewWidget({
-    Key key,
-    @required TabController tabcontroller,
-    @required List<Widget> screens,
-  })  : _tabcontroller = tabcontroller,
+  const TabBarViewWidget(
+      {Key key,
+      @required TabController tabcontroller,
+      @required List<Widget> screens,
+      @required String pageStorageKey})
+      : _tabcontroller = tabcontroller,
         _screens = screens,
+        _pageStorageKey = pageStorageKey,
         super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
         child: TabBarView(
-      key: PageStorageKey("Tabcontroller"),
+      key: PageStorageKey("$_pageStorageKey"),
       controller: _tabcontroller,
       children: _screens,
     ));
