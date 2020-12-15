afff204514d2e6941a548ebf21c2771dd2861a7a
Williano <paawilly17@gmail.com>
2020-12-14 21:56:12 -0600

    Update

--- appetiser_app/lib/features/recipes/ui/recipe_list_screen/recipe_list_screen.dart
+++ appetiser_app/lib/features/recipes/ui/recipe_list_screen/recipe_list_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'package:appetiser_app/shared/widgets/tab_bar_view_widget/tab_bar_view.dart';
 import 'package:appetiser_app/shared/widgets/tab_bar_widget/tab_bar_widget.dart';
 import 'package:appetiser_app/features/recipes/ui/recipe_list_screen/recipe_list_widgets/search_button_widget.dart';
+import 'package:appetiser_app/shared/widgets/recipe_card_widget/recipe_card_widget.dart';
 
 class RecipeListScreen extends StatefulWidget {
   const RecipeListScreen({Key key}) : super(key: key);
@@ -34,7 +36,11 @@ class _RecipeListScreenState extends State<RecipeListScreen>
       child: ListView.builder(
           physics: BouncingScrollPhysics(),
           itemBuilder: (BuildContext context, int index) {
-            return RecipeWidget();
+            return RecipeCardWidget(
+              recipeImage: "assets/images/hamburger.jpg",
+              recipeTitle: "Ginger Marinated Tempeh",
+              recipeSubTitle: "Easy and delicious",
+            );
           }),
     ),
     Container(
@@ -89,108 +95,5 @@ class _RecipeListScreenState extends State<RecipeListScreen>
   void dispose() {
     _tabcontroller.dispose();
     super.dispose();
-  }
-}
-
-class RecipeWidget extends StatefulWidget {
-  final Key key;
-  final String recipeImage;
-  final String recipeTitle;
-  final String recipeSubTitle;
-
-  const RecipeWidget(
-      {this.key, this.recipeImage, this.recipeTitle, this.recipeSubTitle});
-
-  @override
-  _RecipeWidgetState createState() => _RecipeWidgetState();
-}
-
-class _RecipeWidgetState extends State<RecipeWidget> {
-  @override
-  Widget build(BuildContext context) {
-    String recipeImage = widget.recipeImage;
-    String recipeTitle = widget.recipeTitle;
-    String recipeSubTitle = widget.recipeSubTitle;
-
-    return Container(
-      padding: const EdgeInsets.only(left: 15.0, right: 15.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10.0),
-            child: Image.asset(
-              "$recipeImage",
-              fit: BoxFit.cover,
-            ),
-          ),
-          SizedBox(
-            height: 5.0,
-          ),
-          Text(
-            "$recipeTitle",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
-          ),
-          SizedBox(
-            height: 5.0,
-          ),
-          Text("$recipeSubTitle"),
-          SizedBox(
-            height: 5.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Icon(
-                    Icons.watch_later,
-                    color: Colors.grey[500],
-                    size: 16,
-                  ),
-                  SizedBox(
-                    width: 3,
-                  ),
-                  Text("20 mins", style: TextStyle(color: Colors.grey[500])),
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.cleaning_services,
-                    color: Colors.grey[500],
-                    size: 16,
-                  ),
-                  SizedBox(
-                    width: 3,
-                  ),
-                  Text("2 serves", style: TextStyle(color: Colors.grey[500])),
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.wb_twighlight,
-                    color: Colors.grey[500],
-                    size: 16,
-                  ),
-                  SizedBox(
-                    width: 3,
-                  ),
-                  Text(
-                    "350 calories",
-                    style: TextStyle(color: Colors.grey[500]),
-                  ),
-                ],
-              )
-            ],
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-        ],
-      ),
-    );
   }
 }
